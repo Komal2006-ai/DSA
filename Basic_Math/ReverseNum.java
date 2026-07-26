aab453b5769c4863c313af7fe568e7f5655c5b9a
Komal <komaljha1112@gmail.com>
2026-07-26 20:53:11 +0530

    Reversenumber

---
+++ Basic_Math/ReverseNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class ReverseNum
+{
+    static void reverseNum(int n)
+    {
+        int reverse = 0;
+        while(n>0)
+        {
+            int lastdigit = n%10;
+            reverse = (reverse * 10) + lastdigit;
+            n = n/10;
+        }
+        System.out.println("Reversed number: "+ reverse);
+    }
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int n = sc.nextInt();
+        reverseNum(n);
+    }
+}
